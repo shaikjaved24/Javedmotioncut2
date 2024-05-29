0dd056b6b5adf3e4a929168722643df9fbee003b
shaikjaved24 <shaikjaved805@gmail.com>
2024-05-29 18:26:15 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,13 @@
+# Quick Two Line Codes
+
+countOfWords = len("This is best Computer Science Portal".split())
+
+print("Count of Words in the given Sentence:", countOfWords)
+ 
+# Quick One Line Codes
+
+print(len("This is best Computer Science Portal".split()))
+ 
+# Quick One Line Code with User Input
+
+print(len(input("Enter Input:").split()))
